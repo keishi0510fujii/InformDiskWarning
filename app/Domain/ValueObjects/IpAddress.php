04cb286fb62630791f8ec1eb3ed49907b0ac62a7
keishi0510fujii <keishifujii0510@gmail.com>
2020-11-21 10:17:08 +0900

    create/IpAddressValueObject

---
+++ app/Domain/ValueObjects/IpAddress.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Domain\ValueObjects;
+
+
+final class IpAddress
+{
+    private $Value;
+
+    public function __construct(String $value){
+        $this->Value = $value;
+    }
+
+    public function value(){ return $this->value; }
+}
